4a51525fd727d820f14685441f0dc0fc20bf6615
Marvin Soto <marvin@inegua.com>
2019-01-19 17:36:26 -0600

    setup project

---
+++ src/providers/github/git-api.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.github.com";
+
+export const getUserRepos = username => {
+    const url = `${BASE_URL}/users/${username}/repos?per_page=250`;
+
+    return new Promise ((resolve, reject) => {
+        axios.get(url).then((response) => {
+            resolve(response.data)
+        })
+        .catch((err) => {
+            reject(err)
+        })
+    })
+};
+
+export const search = query => {
+    const url = `${BASE_URL}/search/repositories?q=${query}`;
+
+    return new Promise ((resolve, reject) => {
+        axios.get(url).then((response) => {
+            resolve(response.data)
+        }).catch((err) => {
+            reject(err)
+        })
+    })
+};
