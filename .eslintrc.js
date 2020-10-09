1aaff015e9b073c37c842cae15ebb5cc23212778
cabaag <caba9313@gmail.com>
2020-10-09 18:40:35 -0500

    Init environment

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/prop-types': 0,
+    'react/jsx-sort-props': [1, { ignoreCase: true }],
+    'linebreak-style': ['error', 'windows'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        mjs: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/extensions': ['.js', '.mjs', '.jsx', '.js', '.jsx', '.ts', '.tsx'],
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
--- git-api.js
+++ git-api.js
@@ -1,15 +1,13 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const BASE_URL = "https://api.github.com"
-
+const BASE_URL = 'https://api.github.com';
 
 export const getUserRepos = (username) => {
-  const url = `${BASE_URL}/users/${username}/repos?per_page=250`
-  return axios.get(url).then(response => response.data)
-}
+  const url = `${BASE_URL}/users/${username}/repos?per_page=250`;
+  return axios.get(url).then((response) => response.data);
+};
 
 export const search = (query) => {
-    const url = `${BASE_URL}/search/repositories?q=${query}`
-    return axios.get(url).then(response => response.data)
-}
-
+  const url = `${BASE_URL}/search/repositories?q=${query}`;
+  return axios.get(url).then((response) => response.data);
+};
